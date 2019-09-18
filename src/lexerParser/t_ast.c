93bc41d5e372eeef3100c9f8f7aa1ca2daf2787b
fakejoshmeier <joshua.meier2@gmail.com>
2019-09-17 21:10:23 -0700

    Restarting the AST creation. Need to fix

---
+++ src/lexerParser/t_ast.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_ast.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/17 13:46:02 by jmeier            #+#    #+#             */
+/*   Updated: 2019/09/17 21:03:11 by jmeier           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sh.h"
+#include "lexer.h"
+
+/*
+** Run through list of tokens, creating a new leaf and ripping off the
+** remainder of the token list, passing it into the leaf to be parsed.  Done
+** through the magic of recursion!
+*/
+
+int		next_op(t_tkn *token, int op)
+{
+	t_tkn	*tmp;
+
+	tmp = token;
+	while (tmp)
+	{
+		if (tmp->op_type == op)
+			return (1);
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+/*
+** Creates splits based on pipes
+*/
+
+t_ast	*create_pipeline(t_tkn **token)
+{
+	t_ast	*root;
+
+	root = create_leaf(token, OPERATOR);
+	if ((*token)->op_type == PIPE)
+	{
+		if (next_op(*token, PIPE))
+		{
+			while ((*token)->op_type == PIPE)
+			{
+				root = create_node(root, create_leaf(token, WORD),
+				create_leaf(token, OPERATOR));
+			}
+		}
+		else
+			root = create_node(root, create_leaf(token, WORD), 
+			create_leaf(token, OPERATOR)); 
+	}
+	return (root);
+}
+
+/*
+** Creates splits in the tree based on '||' or '&&'
+*/
+
+t_ast	*create_list(t_tkn **token)
+{
+	t_ast	*root;
+
+	root = create_pipeline(token);
+	if ((*token)->op_type == ANDIF || (*token)->op_type == ORIF)
+	{
+		if (next_op(*token, ANDIF) || next_op(*token, ORIF))
+		{
+			while ((*token)->op_type == ANDIF || (*token)->op_type == ORIF)
+				root = create_node(root, create_leaf(token, WORD),
+				create_pipeline(token));
+		}
+		else
+			root = create_node(root, create_leaf(token, WORD),
+			create_pipeline(token));
+	}
+	return (root);
+}
+
+/*
+** These functions will run through the tokens until the next operator, and
+** separate nodes based on pipes, certain operators, etc.
+** This level will split based on separators, i.e. ';' and '&'.
+*/
+
+t_ast	*create_ast(t_tkn **token)
+{
+	t_ast	*root;
+
+	root = create_list(token);
+	if ((*token)->op_type == SEPARATOR)
+	{
+		if (next_op(*token, SEPARATOR))
+		{
+			while ((*token)->op_type == SEPARATOR)
+				root = create_node(root, create_leaf(token, WORD),
+				create_list(token));
+		}
+		else
+			root = create_node(root, create_leaf(token, WORD),
+			create_list(token));
+	}
+	return (root);
+}
---
+++ src/lexerParser/t_ast_util.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_ast_util.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/27 16:29:04 by jmeier            #+#    #+#             */
+/*   Updated: 2019/09/17 21:01:55 by jmeier           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+#include "sh.h"
+
+/*
+** AST related functions.  Adding nodes, handling operator precedence according
+** to shell grammar
+*/
+
+/*
+** On a given leaf, rip out chunks of the linked list that is my tokens list.
+*/
+
+
+t_ast	*create_leaf(t_tkn **token, int delim)
+{
+	t_ast	*new;
+	t_tkn	*tmp;
+
+	if (!(*token))
+		return (NULL);
+	new = ft_memalloc(sizeof(t_ast));
+	new->parent = NULL;
+	new->left = NULL;
+	new->right = NULL;
+	new->type = (*token)->type;
+	new->op_type = (*token)->op_type;
+	new->token = *token;
+	new->end = *token;
+	tmp = (*token)->next;
+	while (tmp->next && tmp->type != delim)
+		tmp = tmp->next;
+	*token = tmp;
+	return (new);
+}
+
+t_ast	*create_node(t_ast *left, t_ast *node, t_ast *right)
+{
+	node->left = left;
+	node->right = right;
+	if (left)
+		left->parent = node;
+	if (right)
+		right->parent = node;
+	return (node);
+}
+
+void	ast_del(t_ast **ast)
+{
+	if (!*ast)
+		return ;
+	ast_del(&(*ast)->left);
+	ast_del(&(*ast)->right);
+	free((*ast)->token);
+	free(*ast);
+	*ast = NULL;
+}
