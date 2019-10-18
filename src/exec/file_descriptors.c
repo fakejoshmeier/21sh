a90f505792844fd22e46e2037738c0cdaa2861bc
fakejoshmeier <joshua.meier2@gmail.com>
2019-10-17 22:53:07 -0700

    Rename

--- src/exec/file_descriptors.c
+++ src/exec/file_descriptors.c
@@ -6,7 +6,7 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/03 11:39:38 by jmeier            #+#    #+#             */
-/*   Updated: 2019/10/10 23:57:50 by jmeier           ###   ########.fr       */
+/*   Updated: 2019/10/17 22:40:24 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,33 @@
 ** https://www.gnu.org/software/bash/manual/html_node/Redirections.html
 **
 ** It makes sense to just handle all the fd related stuff here.
+**
+** I need to tcsetattr with STDERR
 */
 
-// int		ft_agreg_files(t_token *redir)
-// {
-// 	int	fd_dest;
-// 	int	fd_src;
+int		fd_aggregate(t_tkn *token)
+{
+	int		dst_fd;
+	int		src_fd;
 
-// 	fd_dest = ft_atoi(redir->next->str);
-// 	if (redir->prev && redir->prev->token_type == IO_NUMBER)
-// 		fd_src = ft_atoi(redir->prev->str);
-// 	else if (redir->operator_type == LESS_AND)
-// 		fd_src = STDIN_FILENO;
-// 	else
-// 		fd_src = STDOUT_FILENO;
-// 	if (ft_isnumber(redir->next->str))
-// 		return (ft_make_dup2(redir->next->str, fd_dest, fd_src));
-// 	else if (ft_strequ(redir->next->str, "-"))
-// 	{
-// 		close(fd_src);
-// 		return (REDIR_OK);
-// 	}
-// 	else
-// 	{
-// 		ft_put_cmd_error(redir->next->str, STR_AMB_REDIR);
-// 		return (REDIR_ERROR);
-// 	}
-// }
+	src_fd = token->op_type == LESSAND ? STDIN_FILENO : STDOUT_FILENO;
+	if (token->op_type == IONUMBER && token->next)
+	{
+		if (token->next->type == REDIRECT)
+			return (fd_redirect(token));
+		src_fd = ft_atoi(token->val);
+		token = token->next;
+	}
+	dst_fd = ft_atoi(token->next->val);
+	ft_printf("%i\n", dst_fd);
+	if (token->next->type == IONUMBER)
+		return (fd_dup2(token->next->val, dst_fd, src_fd));
+	else if (ft_strequ(token->next->val, "-"))
+		return (close(src_fd));
+	else
+		return (redirect_error(1));
+		// return (redirect_error(token->next->val, STR_AMB_REDIR));
+}
 
 int		fd_dup2(char *str, int dst_fd, int src_fd)
 {
@@ -54,26 +54,22 @@ int		fd_dup2(char *str, int dst_fd, int src_fd)
 	else
 	{
 		if (errno == EBADF)
-			ERROR_PROMPT(BAD_FD_ERR, str);
+			ERR_P(BAD_FD_ERR, str);
 		else
-			ERROR_PROMPT(DUP_ERR, str);
+			ERR_P(DUP_ERR, str);
 		return (1);
 	}
 }
-// Work on the IO Number stuff later on
+
 int		fd_redirect(t_tkn *token)
 {
 	int		src_fd;
-	t_tkn	*tmp;
 
 	errno = 0;
 	if (token->type == IONUMBER)
 	{
 		src_fd = ft_atoi(token->val);
-		tmp = token;
-		token = tmp->next;
-		tmp->next = NULL;
-		token_del(&tmp);
+		token = token->next;
 	}
 	else
 		src_fd = token->op_type == LESS ? STDIN_FILENO : STDOUT_FILENO;
@@ -103,21 +99,20 @@ int		set_fd(t_ast *ast, t_sh *sh)
 	sh->fds[0] = dup(STDIN_FILENO);
 	sh->fds[1] = dup(STDOUT_FILENO);
 	sh->fds[2] = dup(STDERR_FILENO);
-	res = 0;
 	tmp = ast->token;
+	if (!tmp)
+		return (1);
+	res = 0;
 	while (tmp)
 	{
-		if (tmp->type == REDIRECT || tmp->type == IO_NUMBER)
-		{
-			if (tmp->op_type == LESSAND || tmp->op_type == GREATAND)
-				res = fd_aggregate(tmp, sh);
-			else if (tmp->op_type == D_LESS || (tmp->type == IO_NUMBER && tmp->next->op_type == D_LESS))
-				res = fd_heredoc(tmp, sh);
-			else
-				res = fd_redirect(tmp);
-			if (res != 0)
-				return (1);
-		}
+		if (tmp->type == AGGREGATE || tmp->type == IONUMBER)
+			res = fd_aggregate(tmp);
+		else if (tmp->op_type == D_LESS)
+			res = fd_heredoc(tmp, sh);
+		else if (tmp->type == REDIRECT)
+			res = fd_redirect(tmp);
+		if (res != 0)
+			return (1);
 		tmp = tmp->next;
 	}
 	return (0);
